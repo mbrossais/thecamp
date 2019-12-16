556f85c45dd9f4b0389b1fa4262dd42e72b99a21
BROSSAIS Mathieu <mbrossais@softwaymedical.fr>
2019-12-16 15:22:35 +0100

    wip

--- src/main/java/com/softwaymedical/sdd2019/thecamp/Groupeur.java
+++ src/main/java/com/softwaymedical/sdd2019/thecamp/Groupeur.java
@@ -3,17 +3,23 @@ package com.softwaymedical.sdd2019.thecamp;
 import com.softwaymedical.sdd2019.thecamp.entity.Equipe;
 import com.softwaymedical.sdd2019.thecamp.entity.Equipes;
 import com.softwaymedical.sdd2019.thecamp.entity.Participants;
+import com.softwaymedical.sdd2019.thecamp.entity.Personne;
 
 public class Groupeur {
 	
 	public Equipes faitDesGroupes(Participants participants) {
 		Equipes equipes = new Equipes();
-		Equipe equipe = new Equipe();
-		participants.getListeParticipant().stream().forEach(participant -> {
-			equipe.getPersonnes().add(participant);
-		});
-		equipes.getEquipes().add(equipe);
+		int nbEquipes = Math.round(participants.getListeParticipant().size() / participants.getNbPersGroup());
+		for (int i = 0; i < nbEquipes; i++) {
+			equipes.getEquipes().add(new Equipe());
+		}
+		int currentEquipeIndex = 0;
+		for (Personne participant : participants.getListeParticipant()) {
+			equipes.getEquipes().get(currentEquipeIndex).getPersonnes().add(participant);
+			currentEquipeIndex = (currentEquipeIndex < nbEquipes - 1) ? currentEquipeIndex + 1 : 0;
+		}
 		return equipes;
 	}
+	
 
 }
