c6830f64fe38258b01481d2cbd868bbe6ec42f92
dimitri <drousseau@softwaymedical.fr>
2019-12-16 14:06:29 +0100

    ajout du groupeur et de la methode pour faire des groupes (vide)

---
+++ src/main/java/com/softwaymedical/sdd2019/thecamp/Groupeur.java
@@ -0,0 +1,12 @@
+package com.softwaymedical.sdd2019.thecamp;
+
+import com.softwaymedical.sdd2019.thecamp.entity.Equipes;
+import com.softwaymedical.sdd2019.thecamp.entity.Participants;
+
+public class Groupeur {
+	
+	public Equipes faitDesGroupes(Participants participants){
+		return null;
+	}
+
+}
---
+++ test/test/com/softwaymedical/sdd2019/thecamp/GroupeurTest.java
@@ -0,0 +1,14 @@
+package test.com.softwaymedical.sdd2019.thecamp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GroupeurTest {
+
+	@Test
+	public void groupeurCreeUneEquipeDeUnePersonne() {
+		
+	}
+
+}
