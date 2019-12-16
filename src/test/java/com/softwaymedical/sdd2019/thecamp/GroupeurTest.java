37e663041249d7d46f5acf2bedca84e1976d6688
dimitri <drousseau@softwaymedical.fr>
2019-12-16 17:26:21 +0100

    Ajout tests agence de rattachement et squads

--- src/test/java/com/softwaymedical/sdd2019/thecamp/GroupeurTest.java
+++ src/test/java/com/softwaymedical/sdd2019/thecamp/GroupeurTest.java
@@ -24,19 +24,30 @@ public class GroupeurTest {
 	private static final String LEADTECH = "TECHLEAD";
 	private static final String DEVELOPPEUR = "DEVELOPPEUR";
 	private static final String SQUAD_RIS = "RIS";
+	private static final String SQUAD_GAP = "GAP";
+	private static final String SQUAD_IMG = "IMG";
+	private static final String SQUAD_ITO = "ITO";
 	private static final String SEXE_M = "M";
 	private static final String SEXE_F = "F";
 	private static final String VILLE_LYON = "Lyon";
-	private static final Personne TECHLEAD_H_LYON = new Personne(NOM, PC_PORTABLE, PRENOM, LEADTECH, SQUAD_RIS, SEXE_M, VILLE_LYON);
-	private static final Personne TECHLEAD_F_LYON = new Personne(NOM, PC_PORTABLE, PRENOM, LEADTECH, SQUAD_RIS, SEXE_F, VILLE_LYON);
-	private static final Personne DEV_H_LYON = new Personne(NOM, PC_PORTABLE, PRENOM, DEVELOPPEUR, SQUAD_RIS, SEXE_M, VILLE_LYON);
-	private static final Personne DEV_F_LYON = new Personne(NOM, PC_PORTABLE, PRENOM, DEVELOPPEUR, SQUAD_RIS, SEXE_F, VILLE_LYON);
+	private static final String VILLE_MEY = "Meyreuil";
+	private static final Personne TECHLEAD_H_LYON_RIS = new Personne(NOM, PC_PORTABLE, PRENOM, LEADTECH, SQUAD_RIS, SEXE_M, VILLE_LYON);
+	private static final Personne TECHLEAD_F_LYON_RIS = new Personne(NOM, PC_PORTABLE, PRENOM, LEADTECH, SQUAD_RIS, SEXE_F, VILLE_LYON);
+	private static final Personne DEV_H_LYON_RIS = new Personne(NOM, PC_PORTABLE, PRENOM, DEVELOPPEUR, SQUAD_RIS, SEXE_M, VILLE_LYON);
+	private static final Personne DEV_F_LYON_RIS = new Personne(NOM, PC_PORTABLE, PRENOM, DEVELOPPEUR, SQUAD_RIS, SEXE_F, VILLE_LYON);
+	private static final Personne TECHLEAD_H_MEY_RIS = new Personne(NOM, PC_PORTABLE, PRENOM, LEADTECH, SQUAD_RIS, SEXE_M, VILLE_LYON);
+	private static final Personne TECHLEAD_F_MEY_RIS = new Personne(NOM, PC_PORTABLE, PRENOM, LEADTECH, SQUAD_RIS, SEXE_F, VILLE_LYON);
+	private static final Personne DEV_H_MEY_RIS = new Personne(NOM, PC_PORTABLE, PRENOM, DEVELOPPEUR, SQUAD_RIS, SEXE_M, VILLE_LYON);
+	private static final Personne DEV_H_MEY_ITO = new Personne(NOM, PC_PORTABLE, PRENOM, DEVELOPPEUR, SQUAD_ITO, SEXE_M, VILLE_LYON);
+	private static final Personne DEV_H_MEY_GAP = new Personne(NOM, PC_PORTABLE, PRENOM, DEVELOPPEUR, SQUAD_GAP, SEXE_M, VILLE_LYON);
+	private static final Personne DEV_H_MEY_IMG = new Personne(NOM, PC_PORTABLE, PRENOM, DEVELOPPEUR, SQUAD_IMG, SEXE_M, VILLE_LYON);
+	private static final Personne DEV_F_MEY_RIS = new Personne(NOM, PC_PORTABLE, PRENOM, DEVELOPPEUR, SQUAD_RIS, SEXE_F, VILLE_LYON);
 	private static final Groupeur groupeur = new Groupeur();
 
 	@Test
 	public void groupeurCreeUneEquipeDeUnePersonne() {
 		List<Personne> listeDesParticipants = new ArrayList<Personne>(1);
-		listeDesParticipants.add(TECHLEAD_H_LYON);
+		listeDesParticipants.add(TECHLEAD_H_LYON_RIS);
 		Participants participants = new Participants(1, listeDesParticipants);
 		assertThat(groupeur.faitDesGroupes(participants).getEquipes().size(), is(1)); // une seule equipe
 		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(0).getPersonnes().size(), is(1)); // une seule personne dans l'equipe
@@ -46,10 +57,10 @@ public class GroupeurTest {
 	public void groupeurCreeDeuxEquipesDeDeuxPersonnes(){
 		List<Personne> listeDesParticipants = new ArrayList<Personne>(4);
 		Participants participants = new Participants(2, listeDesParticipants);
-		listeDesParticipants.add(TECHLEAD_H_LYON);
-		listeDesParticipants.add(TECHLEAD_H_LYON);
-		listeDesParticipants.add(TECHLEAD_H_LYON);
-		listeDesParticipants.add(TECHLEAD_H_LYON);
+		listeDesParticipants.add(TECHLEAD_H_LYON_RIS);
+		listeDesParticipants.add(TECHLEAD_H_LYON_RIS);
+		listeDesParticipants.add(TECHLEAD_H_LYON_RIS);
+		listeDesParticipants.add(TECHLEAD_H_LYON_RIS);
 		assertThat(groupeur.faitDesGroupes(participants).getEquipes().size(), is(2)); // deux equipes
 		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(0).getPersonnes().size(), is(2)); // deux personnes dans l'equipe
 	}
@@ -58,18 +69,18 @@ public class GroupeurTest {
 	public void groupeurCreeEquipesAvecAuMoinsUnTechLead(){
 		List<Personne> listeDesParticipants = new ArrayList<Personne>(12);
 		Participants participants = new Participants(4, listeDesParticipants);
-		listeDesParticipants.add(TECHLEAD_H_LYON);
-		listeDesParticipants.add(TECHLEAD_H_LYON);
-		listeDesParticipants.add(TECHLEAD_H_LYON);
-		listeDesParticipants.add(TECHLEAD_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
+		listeDesParticipants.add(TECHLEAD_H_LYON_RIS);
+		listeDesParticipants.add(TECHLEAD_H_LYON_RIS);
+		listeDesParticipants.add(TECHLEAD_H_LYON_RIS);
+		listeDesParticipants.add(TECHLEAD_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
 		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(0).getNombreTechleads(), is(Matchers.greaterThanOrEqualTo(1)));
 		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(1).getNombreTechleads(), is(Matchers.greaterThanOrEqualTo(1)));
 		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(2).getNombreTechleads(), is(Matchers.greaterThanOrEqualTo(1)));
@@ -79,18 +90,18 @@ public class GroupeurTest {
 	public void groupeurCreeEquipesAvecAuMoinsUnTechLead2(){
 		List<Personne> listeDesParticipants = new ArrayList<Personne>(12);
 		Participants participants = new Participants(4, listeDesParticipants);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(DEV_H_LYON);
-		listeDesParticipants.add(TECHLEAD_H_LYON);
-		listeDesParticipants.add(TECHLEAD_H_LYON);
-		listeDesParticipants.add(TECHLEAD_H_LYON);
-		listeDesParticipants.add(TECHLEAD_H_LYON);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(DEV_H_LYON_RIS);
+		listeDesParticipants.add(TECHLEAD_H_LYON_RIS);
+		listeDesParticipants.add(TECHLEAD_H_LYON_RIS);
+		listeDesParticipants.add(TECHLEAD_H_LYON_RIS);
+		listeDesParticipants.add(TECHLEAD_H_LYON_RIS);
 		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(0).getNombreTechleads(), is(greaterThanOrEqualTo(1)));
 		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(1).getNombreTechleads(), is(greaterThanOrEqualTo(1)));
 		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(2).getNombreTechleads(), is(greaterThanOrEqualTo(1)));
@@ -99,11 +110,47 @@ public class GroupeurTest {
 	@Test
 	public void groupeurCreeEquipesAvecLeBonNombreDeFilles(){
 		List<Personne> listeDesParticipants = new ArrayList<Personne>(40);
-		for (int i = 0; i < 15; i++) {listeDesParticipants.add(TECHLEAD_F_LYON);}
-		for (int i = 0; i < 25; i ++){listeDesParticipants.add(DEV_H_LYON);}
+		for (int i = 0; i < 15; i++) {listeDesParticipants.add(TECHLEAD_F_LYON_RIS);}
+		for (int i = 0; i < 25; i ++){listeDesParticipants.add(DEV_H_LYON_RIS);}
 		Participants participants = new Participants(4, listeDesParticipants);
 		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(0).getNombreFemmes(), greaterThanOrEqualTo(1));
 		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(3).getNombreFemmes(), lessThanOrEqualTo(2));
 	}
+	
+	@Test
+	public void groupeurCreeEquipesAvecLeBonNombreDeFilles2(){
+		List<Personne> listeDesParticipants = new ArrayList<Personne>(40);
+		for (int i = 0; i < 5; i++) {listeDesParticipants.add(TECHLEAD_F_LYON_RIS);}
+		for (int i = 0; i < 10; i++) {listeDesParticipants.add(DEV_F_LYON_RIS);}
+		for (int i = 0; i < 25; i ++){listeDesParticipants.add(DEV_H_LYON_RIS);}
+		Participants participants = new Participants(4, listeDesParticipants);
+		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(0).getNombreFemmes(), greaterThanOrEqualTo(1));
+		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(3).getNombreFemmes(), lessThanOrEqualTo(2));
+	}
+	
+	@Test
+	public void groupeurCreeEquipesRepartiesParAgences(){
+		List<Personne> listeDesParticipants = new ArrayList<Personne>(40);
+		for (int i = 0; i < 10; i++) {listeDesParticipants.add(TECHLEAD_F_MEY_RIS);}
+		for (int i = 0; i < 10; i++) {listeDesParticipants.add(TECHLEAD_F_LYON_RIS);}
+		for (int i = 0; i < 15; i ++){listeDesParticipants.add(DEV_H_MEY_RIS);}
+		for (int i = 0; i < 5; i ++){listeDesParticipants.add(DEV_H_LYON_RIS);}
+		Participants participants = new Participants(10, listeDesParticipants);
+		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(0).getNombreLyon(), is(5));
+	}
+	
+	@Test
+	public void groupeurCreeEquipesRepartiesParSquad(){
+		List<Personne> listeDesParticipants = new ArrayList<Personne>(40);
+		for (int i = 0; i < 5; i++) {listeDesParticipants.add(TECHLEAD_F_MEY_RIS);}
+		for (int i = 0; i < 20; i++) {listeDesParticipants.add(DEV_H_MEY_ITO);}
+		for (int i = 0; i < 10; i ++){listeDesParticipants.add(DEV_H_MEY_GAP);}
+		for (int i = 0; i < 5; i ++){listeDesParticipants.add(DEV_H_MEY_IMG);}
+		Participants participants = new Participants(10, listeDesParticipants);
+		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(0).getNombreSquad(SQUAD_IMG), is(greaterThanOrEqualTo(2)));
+		assertThat(groupeur.faitDesGroupes(participants).getEquipes().get(0).getNombreSquad(SQUAD_ITO), is(5));
+	}
+	
+	
 
 }
